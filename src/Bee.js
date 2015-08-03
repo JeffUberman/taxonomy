f8cfd2684817e7b0e8c82f5395aafb86df8bed1d
MKS-students <mks.students@hackreactor.com>
2015-08-03 14:01:30 -0500

    complete Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,9 +1,15 @@
 // constructor
 var Bee = function() {
   Grub.call(Bee); // or Grub.call(this);
-  this.age = 5;
-  this.color = 'yellow';
-  this.job = 'keep on growing';
+  // this.age = 5;
+  // this.color = 'yellow';
+  // this.job = 'keep on growing';
 };
 
 Bee.prototype = new Grub();
+
+Bee.prototype.constructor = Bee;
+
+Bee.prototype.age = 5;
+Bee.prototype.color = 'yellow';
+Bee.prototype.job = 'keep on growing';
--- src/Grub.js
+++ src/Grub.js
@@ -1,7 +1,10 @@
 var Grub = function() {
-  this.age = 0;
-  this.color = 'pink';
-  this.food = 'jelly';
+  //this.age = 0;
+  //this.color = 'pink';
+  //this.food = 'jelly';
 };
 
 Grub.prototype.eat = function(){};
+Grub.prototype.age = 0;
+Grub.prototype.color = 'pink';
+Grub.prototype.food = 'jelly';
