12cb85eeade91c2e70fef4f4e3b504be3c684182
MKS-students <mks.students@hackreactor.com>
2015-08-03 13:47:28 -0500

    Insert Grub, Build Bee Class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
+// constructor
 var Bee = function() {
+  Grub.call(Bee); // or Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = new Grub();
